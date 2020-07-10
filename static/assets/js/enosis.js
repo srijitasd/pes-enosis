b9e80119eef5bcdebd5fbd9463f306663b250f20
YOUR_USERNAMErroy11705 <rroy11705@gmail.com>
2020-07-11 00:41:30 +0530

    preloader added

--- static/assets/js/enosis.js
+++ static/assets/js/enosis.js
@@ -1,35 +1,41 @@
-gsap.fromTo(".mainlogo", {
+$(window).on('load', function () {
+    $('#preloader').hide("slow");
+    LandingLogoAnimation();
+});
+LandingLogoAnimation = ()=> {
+    gsap.fromTo(".mainlogo", {
+            y: "-1.5vw",
+            opacity: 0,
+        },
+        {
+            y: "0vw",
+            opacity: 1,
+            duration: 0.5,
+            delay: 0.7
+        }
+    )
+
+    gsap.fromTo(".logo-background", {
         y: "-1.5vw",
         opacity: 0,
     },
     {
         y: "0vw",
-        opacity: 1,
-        duration: 0.5,
+        opacity: 0.2,
+        duration: 1,
         delay: 0.7
     }
-)
+    )
 
-gsap.fromTo(".logo-background", {
-    y: "-1.5vw",
-    opacity: 0,
-},
-{
-    y: "0vw",
-    opacity: 0.2,
-    duration: 1,
-    delay: 0.7
+    gsap.fromTo(".logoText", {
+            x: "-3vw",
+            opacity: 0,
+        },
+        {
+            x: "0vw",
+            opacity: 1,
+            duration: 0.7,
+            delay: 0.8
+        }
+    )
 }
-)
-
-gsap.fromTo(".logoText", {
-        x: "-3vw",
-        opacity: 0,
-    },
-    {
-        x: "0vw",
-        opacity: 1,
-        duration: 0.7,
-        delay: 0.8
-    }
-)
