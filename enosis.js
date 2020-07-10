6e849298710484ae105958471dd938331d9c5b80
Srijit Das <srijit29032001@gmail.com>
2020-07-10 20:14:32 +0530

    first commit

---
+++ enosis.js
@@ -0,0 +1,17 @@
+gsap.fromTo(".mainlogo", {y: "-1.5vw",
+                    opacity: 0,
+                            },
+                            {y: "0vw",
+                    opacity: 1,
+                duration: 0.7,
+            delay: 0.5}
+                            )
+
+gsap.fromTo(".logoText", {x: "-3vw",
+                    opacity: 0,
+                            },
+                            {x: "0vw",
+                    opacity: 1,
+                duration: 0.7,
+            delay: 0.6}
+                            )
