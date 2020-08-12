aa8b1856304e99d055b5f101332a85d57a1b97dd
Srijit Das <srijit29032001@gmail.com>
2020-08-13 00:59:53 +0530

    tweeks for hall of fame

--- static/assets/js/tour-page.js
+++ static/assets/js/tour-page.js
@@ -29,29 +29,3 @@ $(document).ready(function(){
     })
 })
 
-$(document).ready(function(){
-    $('#item4').on('click', function(){
-        $(this).addClass('active');
-        $.ajax({url: "/tour-winners/tour-winner4.html", success: function(result){
-            $('.container').html(result);
-        }})
-    })
-})
-
-$(document).ready(function(){
-    $('#item5').on('click', function(){
-        $(this).addClass('active');
-        $.ajax({url: "/tour-winners/tour-winner5.html", success: function(result){
-            $('.container').html(result);
-        }})
-    })
-})
-
-$(document).ready(function(){
-    $('#item6').on('click', function(){
-        $(this).addClass('active');
-        $.ajax({url: "/tour-winners/tour-winner6.html", success: function(result){
-            $('.container').html(result);
-        }})
-    })
-})
---
+++ static/assets/js/tour-pge.js
@@ -0,0 +1,6 @@
+let onGoing =  document.getElementsByClassName("list-items")
+let onGoingList =  document.getElementsByClassName("onGoing")
+
+onGoing.addEventListener("click", showList)
+
+let showList = () => console.log("hello")
